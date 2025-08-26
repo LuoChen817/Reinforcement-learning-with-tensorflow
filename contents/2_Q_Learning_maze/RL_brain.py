5d128d808c6b85accf07a7a4a395c3e58c54d24a
无情+1机器 <270890028@qq.com>
2025-08-26 15:52:15 +0800

    panda.append改为panda.concat

--- contents/2_Q_Learning_maze/RL_brain.py
+++ contents/2_Q_Learning_maze/RL_brain.py
@@ -42,10 +42,7 @@ class QLearningTable:
     def check_state_exist(self, state):
         if state not in self.q_table.index:
             # append new state to q table
-            self.q_table = self.q_table.append(
-                pd.Series(
-                    [0]*len(self.actions),
-                    index=self.q_table.columns,
-                    name=state,
-                )
-            )
+            self.q_table = pd.concat(
+                            [self.q_table,
+                             pd.Series([0]*len(self.actions), index=self.q_table.columns, name=state).to_frame().T]
+)
